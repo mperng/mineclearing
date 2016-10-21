ccb9bb05e5172e8b16d64dc963be2d85d3f0f73a
themsg <michaelperng@gmail.com>
2016-10-21 17:36:13 -0500

    shift numMines to Cuboid class, updated unit tests

--- mineclearing.py
+++ mineclearing.py
@@ -1,13 +1,17 @@
 class Game(object):
-    def __init__(self, numMines):
-        self.cuboid = Cuboid()
-        self.steps = []
+    def __init__(self, cuboidStr):
+        self.scriptSteps = []
         self.volleysFired = 0
-        self.shipPosition = None
-        self.numMines = numMines
-        self.score = (10 * numMines)
+        self.cuboid = Cuboid(cuboidStr)
+        self.score = (10 * self.cuboid.numMines)
 
 
 class Cuboid(object):
-    def __init__(self):
-        pass
+    def __init__(self, cuboidStr):
+        # assuming the same number of cols for each row,
+        # otherwise invalid cuboid given
+        self.grid, self.numMines = [], 0
+        for line in cuboidStr.split():
+            l = list(line)
+            self.numMines += len(l) - l.count('.')
+            self.grid.append(l)
--- test_mineclearing.py
+++ test_mineclearing.py
@@ -5,19 +5,22 @@ from mineclearing import *
 
 class CuboidTest(unittest.TestCase):
     def testCuboidInit1(self):
-        c = Cuboid()
+        cuboidStr = '..Z..\n.Z..Z\n.ZZZ.\n'
+        c = Cuboid(cuboidStr)
+        expected_grid = [list(line) for line in cuboidStr.split()]
+        expected_numMines = 6
+        self.assertEqual(c.grid, expected_grid)
+        self.assertEqual(c.numMines, expected_numMines)
 
 
 class GameTest(unittest.TestCase):
     def testGameInit1(self):
-        g = Game(0)
-        self.assertEqual(g.numMines, 0)
+        g = Game('')
         self.assertEqual(g.score, 0)
 
     def testGameInit2(self):
-        g = Game(1)
-        self.assertEqual(g.numMines, 1)
-        self.assertEqual(g.score, 10)
+        g = Game('..Z..\n.Z..Z\n.ZZZ.\n')
+        self.assertEqual(g.score, 60)
 
 if __name__ == '__main__':
     unittest.main()
