97a4df5b71c85924b82a5afd7a4fd17c54d158a8
themsg <michaelperng@gmail.com>
2016-10-22 04:56:28 -0500

    Added command line argv parser to mineclearing.py, changed name of Game class to Simulation, added functions to Simulation and Cuboid, updated Cuboid attributes

--- mineclearing.py
+++ mineclearing.py
@@ -1,17 +1,61 @@
-class Game(object):
+class Simulation(object):
+    '''
+    A class representing a simulation of a mine-clearing script
+    '''
     def __init__(self, cuboidStr):
         self.scriptSteps = []
         self.volleysFired = 0
         self.cuboid = Cuboid(cuboidStr)
-        self.score = (10 * self.cuboid.numMines)
+        self.score = 10 * self.cuboid.numMines
+
+    def run(self):
+        '''
+        Run the script until an end condition has been reached
+        '''
+        pass
+
+    def evaluate(self):
+        '''
+        Evaluate if an end condition has been reached.
+        '''
+        pass
 
 
 class Cuboid(object):
+    '''
+    A class representing a cuboid view in the simulation
+    '''
     def __init__(self, cuboidStr):
-        # assuming the same number of cols for each row,
-        # otherwise invalid cuboid given
-        self.grid, self.numMines = [], 0
-        for line in cuboidStr.split():
-            l = list(line)
-            self.numMines += len(l) - l.count('.')
-            self.grid.append(l)
+        self.grid = [list(l) for l in cuboidStr.split()]
+        self.minePosList = self._getMinePositions()
+        self.numRows = len(self.grid)
+        self.numCols = len(self.grid[0])
+
+    def _getMinePositions(self):
+        '''
+        Return a list of positions (row_num, col_num) where mines are located
+        '''
+        minePosList = []
+        for x_pos, row in enumerate(self.grid):
+            minePosList += reduce(lambda l, t: l + [[x_pos, t[0]]],
+                                  filter(lambda t: t[1] != '.', enumerate(row)),
+                                  [])
+        return minePosList
+
+    def executeFireCmd(self, firePositions):
+        '''
+        modify the cuboid to reflect the result of a fire command
+        '''
+        pass
+
+    def executeMoveCmd(self, offset_x, offset_y):
+        '''
+        modify the ship position to reflect the result of a move command
+        '''
+        pass
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        raise SystemExit('Usage: python mineclearing.py \
+                         /path/to/cuboid_file.txt /path/to/script_file.txt')
--- test_mineclearing.py
+++ test_mineclearing.py
@@ -8,19 +8,43 @@ class CuboidTest(unittest.TestCase):
         cuboidStr = '..Z..\n.Z..Z\n.ZZZ.\n'
         c = Cuboid(cuboidStr)
         expected_grid = [list(line) for line in cuboidStr.split()]
-        expected_numMines = 6
+        expected_minePosList = [[0, 2], [1, 1], [1, 4], [2, 1], [2, 2], [2, 3]]
         self.assertEqual(c.grid, expected_grid)
-        self.assertEqual(c.numMines, expected_numMines)
+        self.assertEqual(c.minePosList, expected_minePosList)
+        self.assertEqual(c.numRows, 3)
+        self.assertEqual(c.numCols, 5)
+
+    def testCuboidInit2(self):
+        cuboidStr = '.....\n.Z..Z\n.ZZZ.\n'
+        c = Cuboid(cuboidStr)
+        expected_grid = [list(line) for line in cuboidStr.split()]
+        expected_minePosList = [[1, 1], [1, 4], [2, 1], [2, 2], [2, 3]]
+        self.assertEqual(c.grid, expected_grid)
+        self.assertEqual(c.minePosList, expected_minePosList)
+        self.assertEqual(c.numRows, 3)
+        self.assertEqual(c.numCols, 5)
+
+    def testCuboidInit3(self):
+        cuboidStr = '.....\n.a.b.\n.z....\n.d.c.\n..z..\n'
+        c = Cuboid(cuboidStr)
+        expected_grid = [list(line) for line in cuboidStr.split()]
+        expected_minePosList = [[1, 1], [1, 3], [2, 1], [3, 1], [3, 3], [4, 2]]
+        self.assertEqual(c.grid, expected_grid)
+        self.assertEqual(c.minePosList, expected_minePosList)
+        self.assertEqual(c.numRows, 5)
+        self.assertEqual(c.numCols, 5)
 
 
-class GameTest(unittest.TestCase):
-    def testGameInit1(self):
-        g = Game('')
-        self.assertEqual(g.score, 0)
+"""
+class SimulationTest(unittest.TestCase):
+    def testSimulationInit1(self):
+        s = Simulation('a')
+        self.assertEqual(s.score, 10)
 
-    def testGameInit2(self):
-        g = Game('..Z..\n.Z..Z\n.ZZZ.\n')
-        self.assertEqual(g.score, 60)
+    def testSimulationInit2(self):
+        s = Simulation('..Z..\n.Z..Z\n.ZZZ.\n')
+        self.assertEqual(s.score, 60)
+"""
 
 if __name__ == '__main__':
     unittest.main()
