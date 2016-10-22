57224905a39ac625dfab76fef7890cf44dab41bc
themsg <michaelperng@gmail.com>
2016-10-22 06:27:58 -0500

    Changed variable names, added inBounds function & unit tests, __str__ method to Cuboid class

--- mineclearing.py
+++ mineclearing.py
@@ -26,21 +26,38 @@ class Cuboid(object):
     A class representing a cuboid view in the simulation
     '''
     def __init__(self, cuboidStr):
-        self.grid = [list(l) for l in cuboidStr.split()]
-        self.minePosList = self._getMinePositions()
-        self.numRows = len(self.grid)
-        self.numCols = len(self.grid[0])
+        self.matrix = [list(l) for l in cuboidStr.split()]
+        self.mines = self._getMinePositions()
+        self.numRows = len(self.matrix)
+        self.numCols = len(self.matrix[0])
+        self.shipPos = [self.numRows / 2, self.numCols / 2]
+        self.minesRemaining = len(self.mines)
 
     def _getMinePositions(self):
         '''
         Return a list of positions (row_num, col_num) where mines are located
         '''
-        minePosList = []
-        for x_pos, row in enumerate(self.grid):
-            minePosList += reduce(lambda l, t: l + [[x_pos, t[0]]],
-                                  filter(lambda t: t[1] != '.', enumerate(row)),
-                                  [])
-        return minePosList
+        mines = []
+        for x_pos, row in enumerate(self.matrix):
+            mines += reduce(lambda l, t: l + [[x_pos, t[0]]],
+                            filter(lambda t: t[1] != '.', enumerate(row)),
+                            [])
+        return mines
+
+    def inBounds(self, x, y):
+        '''
+        Return True if the given x, y values
+        are within the bounds of the cuboid
+        '''
+        if x < 0 or y < 0 or x >= self.numRows or y >= self.numCols:
+            return False
+        return True
+
+    def __str__(self):
+        '''
+        return the string representation of the Cuboid
+        '''
+        return '\n'.join([''.join(x) for x in self.matrix ]) + '\n'
 
     def executeFireCmd(self, firePositions):
         '''
--- test_mineclearing.py
+++ test_mineclearing.py
@@ -7,32 +7,91 @@ class CuboidTest(unittest.TestCase):
     def testCuboidInit1(self):
         cuboidStr = '..Z..\n.Z..Z\n.ZZZ.\n'
         c = Cuboid(cuboidStr)
-        expected_grid = [list(line) for line in cuboidStr.split()]
-        expected_minePosList = [[0, 2], [1, 1], [1, 4], [2, 1], [2, 2], [2, 3]]
-        self.assertEqual(c.grid, expected_grid)
-        self.assertEqual(c.minePosList, expected_minePosList)
+        expected_matrix = [list(line) for line in cuboidStr.split()]
+        expected_mines = [[0, 2], [1, 1], [1, 4], [2, 1], [2, 2], [2, 3]]
+        self.assertEqual(c.matrix, expected_matrix)
+        self.assertEqual(c.mines, expected_mines)
         self.assertEqual(c.numRows, 3)
         self.assertEqual(c.numCols, 5)
+        self.assertEqual(c.shipPos, [1, 2])
 
     def testCuboidInit2(self):
         cuboidStr = '.....\n.Z..Z\n.ZZZ.\n'
         c = Cuboid(cuboidStr)
-        expected_grid = [list(line) for line in cuboidStr.split()]
-        expected_minePosList = [[1, 1], [1, 4], [2, 1], [2, 2], [2, 3]]
-        self.assertEqual(c.grid, expected_grid)
-        self.assertEqual(c.minePosList, expected_minePosList)
+        expected_matrix = [list(line) for line in cuboidStr.split()]
+        expected_mines = [[1, 1], [1, 4], [2, 1], [2, 2], [2, 3]]
+        self.assertEqual(c.matrix, expected_matrix)
+        self.assertEqual(c.mines, expected_mines)
         self.assertEqual(c.numRows, 3)
         self.assertEqual(c.numCols, 5)
+        self.assertEqual(c.shipPos, [1, 2])
 
     def testCuboidInit3(self):
-        cuboidStr = '.....\n.a.b.\n.z....\n.d.c.\n..z..\n'
+        cuboidStr = '.....\n.a.b.\n.z...\n.d.c.\n..z..\n'
         c = Cuboid(cuboidStr)
-        expected_grid = [list(line) for line in cuboidStr.split()]
-        expected_minePosList = [[1, 1], [1, 3], [2, 1], [3, 1], [3, 3], [4, 2]]
-        self.assertEqual(c.grid, expected_grid)
-        self.assertEqual(c.minePosList, expected_minePosList)
+        expected_matrix = [list(line) for line in cuboidStr.split()]
+        expected_mines = [[1, 1], [1, 3], [2, 1], [3, 1], [3, 3], [4, 2]]
+        self.assertEqual(c.matrix, expected_matrix)
+        self.assertEqual(c.mines, expected_mines)
         self.assertEqual(c.numRows, 5)
         self.assertEqual(c.numCols, 5)
+        self.assertEqual(c.shipPos, [2, 2])
+
+    def testCuboidInit4(self):
+        cuboidStr = 'a'
+        c = Cuboid(cuboidStr)
+        expected_matrix = [list(line) for line in cuboidStr.split()]
+        expected_mines = [[0, 0]]
+        self.assertEqual(c.matrix, expected_matrix)
+        self.assertEqual(c.mines, expected_mines)
+        self.assertEqual(c.numRows, 1)
+        self.assertEqual(c.numCols, 1)
+        self.assertEqual(c.shipPos, [0, 0])
+
+    def testCuboidInit5(self):
+        cuboidStr = 'a...a'
+        c = Cuboid(cuboidStr)
+        expected_matrix = [list(line) for line in cuboidStr.split()]
+        expected_mines = [[0, 0], [0, 4]]
+        self.assertEqual(c.matrix, expected_matrix)
+        self.assertEqual(c.mines, expected_mines)
+        self.assertEqual(c.numRows, 1)
+        self.assertEqual(c.numCols, 5)
+        self.assertEqual(c.shipPos, [0, 2])
+
+    def testCuboidInit6(self):
+        cuboidStr = '..Z..\n.....\nZ...Z\n.....\n..Z..\n'
+        c = Cuboid(cuboidStr)
+        expected_matrix = [list(line) for line in cuboidStr.split()]
+        expected_mines = [[0, 2], [2, 0], [2, 4], [4, 2]]
+        self.assertEqual(c.matrix, expected_matrix)
+        self.assertEqual(c.mines, expected_mines)
+        self.assertEqual(c.numRows, 5)
+        self.assertEqual(c.numCols, 5)
+        self.assertEqual(c.shipPos, [2, 2])
+
+    def testCuboidInBounds1(self):
+        c = Cuboid('a')
+        self.assertEqual(c.inBounds(-1, -1), False)
+        self.assertEqual(c.inBounds(-1, 0), False)
+        self.assertEqual(c.inBounds(0, -1), False)
+        self.assertEqual(c.inBounds(0, 1), False)
+        self.assertEqual(c.inBounds(1, 0), False)
+        self.assertEqual(c.inBounds(1, 1), False)
+        self.assertEqual(c.inBounds(0, 0), True)
+
+    def testCuboidInBounds2(self):
+        c = Cuboid('..Z..\n.....\nZ...Z\n.....\n..Z..\n')
+        for x in xrange(c.numRows):
+            for y in xrange(c.numCols):
+                self.assertEqual(c.inBounds(x, y), True)
+        self.assertEqual(c.inBounds(-1, -1), False)
+        self.assertEqual(c.inBounds(-1, 0), False)
+        self.assertEqual(c.inBounds(0, -1), False)
+        self.assertEqual(c.inBounds(c.numRows, 0), False)
+        self.assertEqual(c.inBounds(0, c.numCols), False)
+
+
 
 
 """
