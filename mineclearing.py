c9bd4772e38b646c6e83dcf5fc8b3af6b9e22d55
themsg <michaelperng@gmail.com>
2016-10-22 20:15:00 -0500

    Made variable names pep8-compliant, added add_row function, fleshed out args parsing

--- mineclearing.py
+++ mineclearing.py
@@ -2,10 +2,10 @@ class Simulation(object):
     '''
     A class representing a simulation of a mine-clearing script
     '''
-    def __init__(self, cuboidStr):
-        self.scriptSteps = []
-        self.volleysFired = 0
-        self.cuboid = Cuboid(cuboidStr)
+    def __init__(self, cuboid, scriptSteps):
+        self.script_steps = []
+        self.volleys_fired = 0
+        self.cuboid = cuboid
         self.score = 10 * self.cuboid.numMines
 
     def run(self):
@@ -25,54 +25,70 @@ class Cuboid(object):
     '''
     A class representing a cuboid view in the simulation
     '''
-    def __init__(self, cuboidStr):
-        self.matrix = [list(l) for l in cuboidStr.split()]
-        self.mines = self._getMinePositions()
-        self.numRows = len(self.matrix)
-        self.numCols = len(self.matrix[0])
-        self.shipPos = [self.numRows / 2, self.numCols / 2]
-        self.minesRemaining = len(self.mines)
+    def __init__(self, cuboid_str=None):
+        self.matrix, self.mine_positions = [], []
+        self.num_rows, self.num_cols = 0, 0
+        self.mines_remaining = 0
+        self.ship_position = None
 
-    def _getMinePositions(self):
+    def add_row(self, row):
         '''
-        Return a list of positions (row_num, col_num) where mines are located
+        Add a row to the underlying matrix representation of this cuboid.
         '''
-        mines = []
-        for x_pos, row in enumerate(self.matrix):
-            mines += reduce(lambda l, t: l + [[x_pos, t[0]]],
-                            filter(lambda t: t[1] != '.', enumerate(row)),
-                            [])
-        return mines
+        self.matrix.append(row)
+        mines = [(self.num_rows, y[0]) for y in enumerate(row) if y[1] != '.']
+        self.mine_positions += mines
+        self.num_rows += 1
+        self.num_cols = max(self.num_cols, len(row))
+        self.mines_remaining += len(mines)
+        self.ship_position = [self.num_rows / 2, self.num_cols / 2]
 
-    def inBounds(self, x, y):
+    def in_bounds(self, x, y):
         '''
         Return True if the given x, y values
         are within the bounds of the cuboid
         '''
-        if x < 0 or y < 0 or x >= self.numRows or y >= self.numCols:
+        if x < 0 or y < 0 or x >= self.num_rows or y >= self.num_cols:
             return False
         return True
 
-    def __str__(self):
+    def decrement_mine_distance(self, value):
         '''
-        return the string representation of the Cuboid
+        decrement one mine's distance value
         '''
-        return '\n'.join([''.join(x) for x in self.matrix ]) + '\n'
+        pass
 
-    def executeFireCmd(self, firePositions):
+    def fire(self, firePositions):
         '''
         modify the cuboid to reflect the result of a fire command
         '''
         pass
 
-    def executeMoveCmd(self, offset_x, offset_y):
+    def move(self, offset_x, offset_y):
         '''
         modify the ship position to reflect the result of a move command
         '''
         pass
+
+    def __str__(self):
+        '''
+        return the string representation of the Cuboid
+        '''
+        return '\n'.join([''.join(x) for x in self.matrix]) + '\n'
 
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         raise SystemExit('Usage: python mineclearing.py \
                          /path/to/cuboid_file.txt /path/to/script_file.txt')
+    with open(sys.argv[0], 'r'), open(sys.argv[1]) as cuboid_file, script_file:
+        c = Cuboid()
+        cuboid_row = next(cuboid_file, None)
+        while cuboid_row:
+            c.add_row(cuboid_row)
+            cuboid_row = next(cuboid_file, None)
+        s = Simulation(c)
+        script_step = next(script_file, None)
+        while script_step:
+            s.run(script_step.split(' '))
+            script_step = next(script_file, None)
--- test_mineclearing.py
+++ test_mineclearing.py
@@ -4,106 +4,113 @@ from mineclearing import *
 
 
 class CuboidTest(unittest.TestCase):
+    def createCuboid(self, cuboid_str):
+        c = Cuboid()
+        for row in map(list, cuboid_str.split()):
+            c.add_row(row)
+        return c
+
     def testCuboidInit1(self):
-        cuboidStr = '..Z..\n.Z..Z\n.ZZZ.\n'
-        c = Cuboid(cuboidStr)
-        expected_matrix = [list(line) for line in cuboidStr.split()]
-        expected_mines = [[0, 2], [1, 1], [1, 4], [2, 1], [2, 2], [2, 3]]
+        c = Cuboid()
+        self.assertEqual(c.matrix, [])
+        self.assertEqual(c.mine_positions, [])
+        self.assertEqual(c.num_rows, 0)
+        self.assertEqual(c.num_cols, 0)
+        self.assertEqual(c.ship_position, None)
+
+    def testCuboidAddRow1(self):
+        cuboid_str = '..Z..\n.Z..Z\n.ZZZ.\n'
+        c = self.createCuboid(cuboid_str)
+        expected_matrix = [list(line) for line in cuboid_str.split()]
+        expected_mines = [(0, 2), (1, 1), (1, 4), (2, 1), (2, 2), (2, 3)]
         self.assertEqual(c.matrix, expected_matrix)
-        self.assertEqual(c.mines, expected_mines)
-        self.assertEqual(c.numRows, 3)
-        self.assertEqual(c.numCols, 5)
-        self.assertEqual(c.shipPos, [1, 2])
+        self.assertEqual(c.mine_positions, expected_mines)
+        self.assertEqual(c.mines_remaining, 6)
+        self.assertEqual(c.num_rows, 3)
+        self.assertEqual(c.num_cols, 5)
+        self.assertEqual(c.ship_position, [1, 2])
 
-    def testCuboidInit2(self):
-        cuboidStr = '.....\n.Z..Z\n.ZZZ.\n'
-        c = Cuboid(cuboidStr)
-        expected_matrix = [list(line) for line in cuboidStr.split()]
-        expected_mines = [[1, 1], [1, 4], [2, 1], [2, 2], [2, 3]]
+    def testCuboidAddRow2(self):
+        cuboid_str = '.....\n.Z..Z\n.ZZZ.\n'
+        c = self.createCuboid(cuboid_str)
+        expected_matrix = [list(line) for line in cuboid_str.split()]
+        expected_mines = [(1, 1), (1, 4), (2, 1), (2, 2), (2, 3)]
         self.assertEqual(c.matrix, expected_matrix)
-        self.assertEqual(c.mines, expected_mines)
-        self.assertEqual(c.numRows, 3)
-        self.assertEqual(c.numCols, 5)
-        self.assertEqual(c.shipPos, [1, 2])
+        self.assertEqual(c.mine_positions, expected_mines)
+        self.assertEqual(c.mines_remaining, 5)
+        self.assertEqual(c.num_rows, 3)
+        self.assertEqual(c.num_cols, 5)
+        self.assertEqual(c.ship_position, [1, 2])
 
-    def testCuboidInit3(self):
-        cuboidStr = '.....\n.a.b.\n.z...\n.d.c.\n..z..\n'
-        c = Cuboid(cuboidStr)
-        expected_matrix = [list(line) for line in cuboidStr.split()]
-        expected_mines = [[1, 1], [1, 3], [2, 1], [3, 1], [3, 3], [4, 2]]
+    def testCuboidAddRow3(self):
+        cuboid_str = '.....\n.a.b.\n.z...\n.d.c.\n..z..\n'
+        c = self.createCuboid(cuboid_str)
+        expected_matrix = [list(line) for line in cuboid_str.split()]
+        expected_mines = [(1, 1), (1, 3), (2, 1), (3, 1), (3, 3), (4, 2)]
         self.assertEqual(c.matrix, expected_matrix)
-        self.assertEqual(c.mines, expected_mines)
-        self.assertEqual(c.numRows, 5)
-        self.assertEqual(c.numCols, 5)
-        self.assertEqual(c.shipPos, [2, 2])
+        self.assertEqual(c.mine_positions, expected_mines)
+        self.assertEqual(c.mines_remaining, 6)
+        self.assertEqual(c.num_rows, 5)
+        self.assertEqual(c.num_cols, 5)
+        self.assertEqual(c.ship_position, [2, 2])
 
-    def testCuboidInit4(self):
-        cuboidStr = 'a'
-        c = Cuboid(cuboidStr)
-        expected_matrix = [list(line) for line in cuboidStr.split()]
-        expected_mines = [[0, 0]]
+    def testCuboidaddRow4(self):
+        cuboid_str = 'a'
+        c = self.createCuboid(cuboid_str)
+        expected_matrix = [list(line) for line in cuboid_str.split()]
+        expected_mines = [(0, 0)]
         self.assertEqual(c.matrix, expected_matrix)
-        self.assertEqual(c.mines, expected_mines)
-        self.assertEqual(c.numRows, 1)
-        self.assertEqual(c.numCols, 1)
-        self.assertEqual(c.shipPos, [0, 0])
+        self.assertEqual(c.mine_positions, expected_mines)
+        self.assertEqual(c.mines_remaining, 1)
+        self.assertEqual(c.num_rows, 1)
+        self.assertEqual(c.num_cols, 1)
+        self.assertEqual(c.ship_position, [0, 0])
 
-    def testCuboidInit5(self):
-        cuboidStr = 'a...a'
-        c = Cuboid(cuboidStr)
-        expected_matrix = [list(line) for line in cuboidStr.split()]
-        expected_mines = [[0, 0], [0, 4]]
+    def testCuboidaddRow5(self):
+        cuboid_str = 'a...a'
+        c = self.createCuboid(cuboid_str)
+        expected_matrix = [list(line) for line in cuboid_str.split()]
+        expected_mines = [(0, 0), (0, 4)]
         self.assertEqual(c.matrix, expected_matrix)
-        self.assertEqual(c.mines, expected_mines)
-        self.assertEqual(c.numRows, 1)
-        self.assertEqual(c.numCols, 5)
-        self.assertEqual(c.shipPos, [0, 2])
+        self.assertEqual(c.mine_positions, expected_mines)
+        self.assertEqual(c.mines_remaining, 2)
+        self.assertEqual(c.num_rows, 1)
+        self.assertEqual(c.num_cols, 5)
+        self.assertEqual(c.ship_position, [0, 2])
 
-    def testCuboidInit6(self):
-        cuboidStr = '..Z..\n.....\nZ...Z\n.....\n..Z..\n'
-        c = Cuboid(cuboidStr)
-        expected_matrix = [list(line) for line in cuboidStr.split()]
-        expected_mines = [[0, 2], [2, 0], [2, 4], [4, 2]]
+    def testCuboidaddRow6(self):
+        cuboid_str = '..Z..\n.....\nZ...Z\n.....\n..Z..\n'
+        c = self.createCuboid(cuboid_str)
+        expected_matrix = [list(line) for line in cuboid_str.split()]
+        expected_mines = [(0, 2), (2, 0), (2, 4), (4, 2)]
         self.assertEqual(c.matrix, expected_matrix)
-        self.assertEqual(c.mines, expected_mines)
-        self.assertEqual(c.numRows, 5)
-        self.assertEqual(c.numCols, 5)
-        self.assertEqual(c.shipPos, [2, 2])
+        self.assertEqual(c.mine_positions, expected_mines)
+        self.assertEqual(c.mines_remaining, 4)
+        self.assertEqual(c.num_rows, 5)
+        self.assertEqual(c.num_cols, 5)
+        self.assertEqual(c.ship_position, [2, 2])
 
-    def testCuboidInBounds1(self):
-        c = Cuboid('a')
-        self.assertEqual(c.inBounds(-1, -1), False)
-        self.assertEqual(c.inBounds(-1, 0), False)
-        self.assertEqual(c.inBounds(0, -1), False)
-        self.assertEqual(c.inBounds(0, 1), False)
-        self.assertEqual(c.inBounds(1, 0), False)
-        self.assertEqual(c.inBounds(1, 1), False)
-        self.assertEqual(c.inBounds(0, 0), True)
+    def testCuboidin_bounds1(self):
+        c = self.createCuboid('a')
+        self.assertEqual(c.in_bounds(-1, -1), False)
+        self.assertEqual(c.in_bounds(-1, 0), False)
+        self.assertEqual(c.in_bounds(0, -1), False)
+        self.assertEqual(c.in_bounds(0, 1), False)
+        self.assertEqual(c.in_bounds(1, 0), False)
+        self.assertEqual(c.in_bounds(1, 1), False)
+        self.assertEqual(c.in_bounds(0, 0), True)
 
-    def testCuboidInBounds2(self):
-        c = Cuboid('..Z..\n.....\nZ...Z\n.....\n..Z..\n')
-        for x in xrange(c.numRows):
-            for y in xrange(c.numCols):
-                self.assertEqual(c.inBounds(x, y), True)
-        self.assertEqual(c.inBounds(-1, -1), False)
-        self.assertEqual(c.inBounds(-1, 0), False)
-        self.assertEqual(c.inBounds(0, -1), False)
-        self.assertEqual(c.inBounds(c.numRows, 0), False)
-        self.assertEqual(c.inBounds(0, c.numCols), False)
+    def testCuboidin_bounds2(self):
+        c = self.createCuboid('..Z..\n.....\nZ...Z\n.....\n..Z..\n')
+        for x in xrange(c.num_rows):
+            for y in xrange(c.num_cols):
+                self.assertEqual(c.in_bounds(x, y), True)
+        self.assertEqual(c.in_bounds(-1, -1), False)
+        self.assertEqual(c.in_bounds(-1, 0), False)
+        self.assertEqual(c.in_bounds(0, -1), False)
+        self.assertEqual(c.in_bounds(c.num_rows, 0), False)
+        self.assertEqual(c.in_bounds(0, c.num_cols), False)
 
-
-
-
-"""
-class SimulationTest(unittest.TestCase):
-    def testSimulationInit1(self):
-        s = Simulation('a')
-        self.assertEqual(s.score, 10)
-
-    def testSimulationInit2(self):
-        s = Simulation('..Z..\n.Z..Z\n.ZZZ.\n')
-        self.assertEqual(s.score, 60)
-"""
 
 if __name__ == '__main__':
     unittest.main()
